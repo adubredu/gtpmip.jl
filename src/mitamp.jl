251b92b1aa38261fb888251f10aba795960bbba4
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2021-12-21 18:10:53 -0500

    encoding complete

--- src/mitamp.jl
+++ src/mitamp.jl
@@ -1,9 +1,15 @@
 module mitamp
 
 using PDDL
+using JuMP 
+using Gurobi
 using SymbolicPlanners
+using mutex
 
 include("graph/graph.jl")
+include("solvers/optiplan.jl")
+
+export encode
 
 
 end
---
+++ test/test_encode.jl
@@ -0,0 +1,4 @@
+using Revise
+using mitamp
+
+model = encode("test/pddl/domain.pddl", "test/pddl/problem.pddl")
